9b8858a20bc0aa6a40bfb174037c7ee4fd797c7e
Luke Edwards <luke.edwards05@gmail.com>
2023-10-14 13:10:06 -0700

    chore: add example dir

---
+++ example/greendale.js
@@ -0,0 +1,10 @@
+exports['GET https://jsonplaceholder.typicode.com/comments?postId=1'] = {"method":"GET","url":"https://jsonplaceholder.typicode.com/comments?postId=1","parameters":[],"headers":[]};
+
+exports['GET https://jsonplaceholder.typicode.com/todos'] = {"method":"GET","url":"https://jsonplaceholder.typicode.com/todos","parameters":[],"headers":[]};
+
+exports['GET https://jsonplaceholder.typicode.com/posts/1'] = {"method":"GET","url":"https://jsonplaceholder.typicode.com/posts/1","parameters":[],"headers":[]};
+
+exports['POST https://jsonplaceholder.typicode.com/posts'] = {"method":"POST","url":"https://jsonplaceholder.typicode.com/posts","parameters":[],"headers":[{"key":"Content-Type","value":"application/json","enable":true}],"body":"{\n  \"title\": \"hello greendale\"\n}"};
+
+exports['GET https://jsonplaceholder.typicode.com/comments'] = {"method":"GET","url":"https://jsonplaceholder.typicode.com/comments","parameters":[{"key":"postId","value":"1","enable":true}],"headers":[]};
+
